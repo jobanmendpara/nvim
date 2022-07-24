ff1a668372cd06be75b7b6b6134bc300774513d3
Joban Mendpara <jobanmendpara@gmail.com>
2022-07-24 16:01:22 -0500

    lsp update

--- lua/user/plugins/lsp/init.lua
+++ lua/user/plugins/lsp/init.lua
@@ -1,8 +1,4 @@
-local status_ok, lspconfig = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
 require('user.plugins.lsp.lsp-installer')
 require('user.plugins.lsp.handlers').setup()
 require('user.plugins.lsp.keymaps')
+require('user.plugins.lsp.lsp-config')
---
+++ lua/user/plugins/lsp/lsp-config.lua
@@ -0,0 +1,12 @@
+local status_ok, lspconfig = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+local handlers = require('user.plugins.lsp.handlers')
+
+lspconfig.sumneko_lua.setup({ capabilities = handlers.capabilities })
+lspconfig.tsserver.setup({ capabilities = handlers.capabilities })
+lspconfig.eslint.setup({ capabilities = handlers.capabilities })
+lspconfig.vls.setup({ capabilities = handlers.capabilities })
+lspconfig.volar.setup({ capabilities = handlers.capabilities })
