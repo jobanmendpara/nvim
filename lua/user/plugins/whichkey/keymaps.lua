034441a01a494fb970c58100d211dbcef8bc48f7
Joban Mendpara <jobanmendpara@gmail.com>
2022-07-25 15:18:50 -0500

    new whichkey maps

--- lua/user/plugins/whichkey/keymaps.lua
+++ lua/user/plugins/whichkey/keymaps.lua
@@ -13,96 +13,93 @@ local opts = {
     nowait = true -- use `nowait` when creating keymaps
 }
 
+local function lspFloatLine ()
+  vim.diagnostic.open_float({'line'})
+end
+
+local function lspNextDiagnostic ()
+  vim.diagnostic.goto_next()
+end
+
+local function lspPrevDiagnostic ()
+  vim.diagnostic.goto_prev()
+end
+
+local function lspShowDefinition ()
+  vim.lsp.buf.definition()
+end
+
+local function lspShowTypeDefinition()
+  vim.lsp.buf.type_definition()
+end
+
+local function lspShowImplementation()
+  vim.lsp.buf.implementation()
+end
+
+local function lspShowReferences()
+  vim.lsp.buf.references()
+end
+
+local function lspShowCodeAction()
+  vim.lsp.buf.code_action()
+end
+
+local function lspRename()
+  vim.lsp.buf.rename()
+end
+
 local mappings = {
-  new_space_mappings = {
+  space_mappings = {
     c = {'<cmd>Bdelete!<cr>', 'Close Buffer'},
     e = {'<cmd>NvimTreeToggle<cr>', 'Toggle Explorer'},
     f = {builtins.current_buffer_fuzzy_find, 'Current Buffer Fuzzy Finder'},
     F = {builtins.live_grep, 'Find in Project'},
-    ['h'] = {
-      ['1'] = {'<cmd>HopChar1<cr>', 'Hop 1 Char'},
-      ['2'] = {'<cmd>HopChar2<cr>', 'Hop 2 Char'},
-      ['3'] = {'<cmd>HopWord<cr>', 'Hop Word'},
-      ['4'] = {'<cmd>HopLine<cr>', 'Hop Line'},
-      ['5'] = {'<cmd>HopPattern<cr>', 'Hop Pattern'},
-    },
     p = {'<cmd>TroubleToggle<cr>', 'Toggle Problems'},
     s = {'<cmd>vsplit<cr>', 'Vertical Split'},
     S = {'<cmd>hsplit<cr>', 'Horizontal Split'},
     t = {'<cmd>ToggleTerm<cr>', 'Toggle Terminal'},
     w = {'<cmd>w<cr>', 'Save Buffer'},
     W = {'<cmd>wa<cr>', 'Save All Buffers'},
-
+    ['['] = {
+      name = 'LSP',
+      c = {lspShowCodeAction, 'Show Code Actions'},
+      d = {lspShowDefinition, 'Show Definition'},
+      D = {lspShowTypeDefinition, 'Show Type Definition'},
+      e = {lspNextDiagnostic, 'Show Next Diagnostic'},
+      i = {lspShowImplementation, 'Show Implementations'},
+      n = {lspRename, 'Rename'},
+      q = {lspFloatLine, 'Show Line Diagnostics'},
+      r = {lspShowReferences, 'Show References'},
+      w = {lspPrevDiagnostic, 'Show Previous Diagnostic'},
+    },
+    ['/'] = {
+      name = 'Hop',
+      ['1'] = {'<cmd>HopChar1<cr>', 'Hop 1 Char'},
+      ['2'] = {'<cmd>HopChar2<cr>', 'Hop 2 Char'},
+      ['3'] = {'<cmd>HopWord<cr>', 'Hop Word'},
+      ['4'] = {'<cmd>HopLine<cr>', 'Hop Line'},
+      ['5'] = {'<cmd>HopPattern<cr>', 'Hop Pattern'},
+    },
+    [','] = {
+      name = 'Telescope',
+      b = {builtins.buffers, 'Buffers'},
+      c = {builtins.colorscheme, 'Colorschemes'},
+      f = {builtins.find_files, 'Files'},
+      F = {builtins.oldfiles, 'Recent Files'},
+      o = {builtins.vim_options, 'Options'},
+      r = {builtins.registers, 'Registers'},
+      R = {builtins.resume, 'Resume'},
+      s = {builtins.search_history, 'Search History'},
+      t = {builtins.treesitter, 'Treesitter'},
+    },
   },
-  -- space_mappings = {
-  --   ['b'] = {
-  --     name = 'Buffer',
-  --     c = {'<cmd>Bdelete!<cr>', 'Close Buffer'},
-  --     C = {'<cmd>BufferLinePickClose<cr>', 'Pick Close'},
-  --     p = {'<cmd>BufferLinePick<cr>', 'Pick'},
-  --   },
-  --   ['f'] = {
-  --     name = 'Find',
-  --     b = {builtins.buffers, 'Buffers'},
-  --     f = {builtins.find_files, 'File'},
-  --     o = {builtins.oldfiles, 'Old File'},
-  --     ['g'] = {
-  --       name = 'Git',
-  --       f = {builtins.git_files, 'Git Files'},
-  --       c = {builtins.git_commits, 'Git Commits'},
-  --       b = {builtins.git_branch, 'Git Branch'},
-  --       s = {builtins.git_status, 'Git Status'},
-  --       S = {builtins.git_stash, 'Git Stash'},
-  --     },
-  --     ['l'] = {
-  --       name = 'LSP',
-  --       d = {builtins.lsp_diagnostics, 'Diagnostics'},
-  --       i = {builtins.lsp_implementations, 'Implementations'},
-  --       ['D'] = {
-  --         name = 'Definitions',
-  --         d = {builtins.lsp_type_definitions, 'Definitions'},
-  --         D = {builtins.lsp_type_definitions, 'Type Definitions'}
-  --       },
-  --       r = {builtins.lsp_references, 'References'},
-  --       ['s'] = {
-  --         name = 'Symbols',
-  --         d = {builtins.lsp_document_symbols, 'Document Symbols'},
-  --         w = {builtins.lsp_workspace_symbols, 'Workspace Symbols'},
-  --         W = {builtins.lsp_workspace_symbols, 'Dynamic Workspace Symbols'},
-  --       },
-  --     },
-  --     ['n'] = {
-  --       name = 'Neovim',
-  --       c = {builtins.commands, 'Command'},
-  --       C = {builtins.colorscheme, 'Colorscheme'},
-  --       h = {builtins.command_history, 'Command History'},
-  --       H = {builtins.help_tags, 'Help Tags'},
-  --       k = {builtins.keymaps, 'Keymaps'},
-  --       m = {builtins.map_pages, 'Manual'},
-  --       o = {builtins.vim_options, 'Options'},
-  --     },
-  --     p = {builtins.pickers, 'Pickers'},
-  --     r = {builtins.registers, 'Registers'},
-  --     R = {builtins.resume, 'Resume'},
-  --     ['s'] = {
-  --       name = 'Search',
-  --       i = {builtins.current_buffer_fuzzy_find, 'Current Buffer Fuzzy Finder'},
-  --       s = {builtins.grep_string, 'Grep String'},
-  --       S = {builtins.live_grep, 'Live Grep'},
-  --       h = {builtins.search_history, 'Search History'},
-  --       t = {builtins.tags, 'Tags'},
-  --     },
-  --     t = {builtins.treesitter, 'Treesitter'},
-  --   },
-  --   w = {'<cmd>w<cr>', 'Save Buffer'},
-  --   q = {'<cmd>q<cr>', 'Quit Window'},
-  -- },
   popup_mappings = {
       scroll_down = '<C-d>',
       scroll_up = '<C-u>'
   }
 }
 
-which_key.register(mappings.new_space_mappings, opts)
+which_key.register(mappings.space_mappings, opts)
 
 return mappings
